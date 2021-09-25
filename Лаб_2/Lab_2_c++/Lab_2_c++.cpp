210636c67055948215d23a47ee64a4fecd6627d0
Buiama <83916242+Buiama@users.noreply.github.com>
2021-09-26 00:08:09 +0300

    Add files via upload

---
+++ Лаб_2/Lab_2_c++/Lab_2_c++.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x, y;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	if (abs(y) >= pow(x, 2) && abs(x) >= pow(y, 2)) cout << "\nYes, this point belongs to this plane\n";
+	else cout << "\nNo, this point does not belong to this plane\n";
+}
